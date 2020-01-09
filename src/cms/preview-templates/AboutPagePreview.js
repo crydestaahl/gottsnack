2a3e717810bcdcaa2ae370e910a8ad87989ba461
crydestaahl <kristoffer@oholics.com>
2020-01-09 18:11:09 +0100

    fixin aboutPagePreview.

--- src/cms/preview-templates/AboutPagePreview.js
+++ src/cms/preview-templates/AboutPagePreview.js
@@ -4,7 +4,7 @@ import { AboutPageTemplate } from '../../templates/about-page'
 
 const AboutPagePreview = ({ entry, widgetFor }) => (
   <AboutPageTemplate
-    title={entry.getIn(['data', 'title'])}
+    title={entry.getIn(['data', 'title', 'image'])}
     content={widgetFor('body')}
   />
 )
