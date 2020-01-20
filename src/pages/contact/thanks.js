7fb228107400552581224b070f0ab08bbbed2dbf
Christoffer Rydeståhl <christoffer.rydestahl@arvika.xnse.com>
2020-01-20 20:23:36 +0100

    changed text in contact reply.

--- src/pages/contact/thanks.js
+++ src/pages/contact/thanks.js
@@ -6,8 +6,8 @@ export default () => (
     <section className="section">
       <div className="container">
         <div className="content">
-          <h1>Thank you!</h1>
-          <p>This is a custom thank you page for form submissions</p>
+          <h1>Tack för att du hör av dig!</h1>
+          <p>Vi kanske hörs...</p>
         </div>
       </div>
     </section>
