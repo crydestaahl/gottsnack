1b22eca8f1ce5c59941f4018297f6f50ea16ad7b
Christoffer Rydeståhl <christoffer.rydestahl@arvika.xnse.com>
2020-02-01 11:06:52 +0100

    Hid tags section in staff post.

--- src/templates/staff-post.js
+++ src/templates/staff-post.js
@@ -29,13 +29,13 @@ export const StaffPostTemplate = ({
             <PostContent content={content} />
             {tags && tags.length ? (
               <div style={{ marginTop: `4rem` }}>
-                <h4>Tags</h4>
+               {/* <h4>Tags</h4>*/ } 
                 <ul className="taglist">
-                  {tags.map(tag => (
+                  {/*{tags.map(tag => (
                     <li key={tag + `tag`}>
                       <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
                     </li>
-                  ))}
+                  ))} */}
                 </ul>
               </div>
             ) : null}
