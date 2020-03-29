399c0a18db1fe3a2bbf3454f9cf4681e49cdd7f4
hallalex <hallalex@users.noreply.github.com>
2020-03-29 19:49:22 +0200

    new dimensions headerimage

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -117,7 +117,7 @@ export const aboutPageQuery = graphql`
                title
                image {
                  childImageSharp {
-                   fluid(maxWidth: 2381, maxHeight: 1471, quality: 100) {
+                   fluid(maxWidth: 900, maxHeight: 556, quality: 100) {
                      ...GatsbyImageSharpFluid
                    }
                  }
