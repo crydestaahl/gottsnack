90f5a7e0bb3a0ac2533b35dbe4b00304c0ff18ff
Christoffer Rydeståhl <christoffer.rydestahl@arvika.xnse.com>
2020-06-01 09:21:27 +0200

    added banner img in cms.

--- src/cms/preview-templates/IndexPagePreview.js
+++ src/cms/preview-templates/IndexPagePreview.js
@@ -16,6 +16,7 @@ const IndexPagePreview = ({ entry, getAsset }) => {
         description={data.description}
         intro={data.intro || { blurbs: [] }}
         mainpitch={data.mainpitch || {}}
+        bannerImage={data.bannerImage}
       />
     )
   } else {
